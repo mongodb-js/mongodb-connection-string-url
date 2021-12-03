5355e693d25fef17a8b73399930d604a6d28598a
Anna Henningsen <anna@addaleax.net>
2021-12-03 15:23:58 +0100

    feat: add connection string redaction utilities COMPASS-5308

---
+++ src/redact.ts
@@ -0,0 +1,54 @@
+import ConnectionString, { CommaAndColonSeparatedRecord } from './index';
+
+export function redactValidConnectionString(inputUrl: Readonly<ConnectionString>): ConnectionString {
+  const url = inputUrl.clone();
+  if (url.username || url.password) {
+    url.username = '_credentials_';
+    url.password = '';
+  }
+  if (url.searchParams.has('authMechanismProperties')) {
+    const props = new CommaAndColonSeparatedRecord(url.searchParams.get('authMechanismProperties'));
+    if (props.get('AWS_SESSION_TOKEN')) {
+      props.set('AWS_SESSION_TOKEN', '_credentials_');
+      url.searchParams.set('authMechanismProperties', props.toString());
+    }
+  }
+  if (url.searchParams.has('tlsCertificateKeyFilePassword')) {
+    url.searchParams.set('tlsCertificateKeyFilePassword', '_credentials_');
+  }
+  if (url.searchParams.has('proxyUsername')) {
+    url.searchParams.set('proxyUsername', '_credentials_');
+  }
+  if (url.searchParams.has('proxyPassword')) {
+    url.searchParams.set('proxyPassword', '_credentials_');
+  }
+  return url;
+}
+
+export function redactConnectionString(uri: string): string {
+  try {
+    // If we can parse the connection string, use the more precise
+    // redaction logic.
+    return (new ConnectionString(uri))
+      .redact()
+      .toString()
+      .replace(/_credentials_/g, '<credentials>');
+  } catch {}
+
+  const regexes = [
+    // Username and password
+    /(?<=\/\/)(.*)(?=@)/g,
+    // AWS IAM Session Token as part of query parameter
+    /(?<=AWS_SESSION_TOKEN(:|%3A))([^,&]+)/gi,
+    // tlsCertificateKeyFilePassword query parameter
+    /(?<=tlsCertificateKeyFilePassword=)([^&]+)/gi,
+    // proxyUsername query parameter
+    /(?<=proxyUsername=)([^&]+)/gi,
+    // proxyPassword query parameter
+    /(?<=proxyPassword=)([^&]+)/gi
+  ];
+  regexes.forEach(r => {
+    uri = uri.replace(r, '<credentials>');
+  });
+  return uri;
+}
